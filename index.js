f7fd9caed6d8903fabd8f96dbf6edee03432e551
AngelBotsD <angelperez22448@gmail.com>
2025-12-12 15:03:44 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import fs, { readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileS
 import yargs from 'yargs'
 import { spawn, execSync } from 'child_process'
 import lodash from 'lodash'
-import { yukiJadiBot } from './plugins/jadibot-serbot.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import { tmpdir } from 'os'
