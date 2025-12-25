06eeb362d0687c9210758be1f9f7128b7da2710c
segundocristian219-oss <segundocristian219@gmail.com>
2025-12-24 18:31:56 -0600

    Actualizar kickall.js

--- plugins/owner/kickall.js
+++ plugins/owner/kickall.js
@@ -5,7 +5,7 @@ const handler = async (m, { conn, participants }) => {
 
     // 🔒 Solo estos pueden usar el comando
     const autorizados = [
-        '38354561278087',
+        '217158512549931',
         '25856038715509',
         ''
     ]
