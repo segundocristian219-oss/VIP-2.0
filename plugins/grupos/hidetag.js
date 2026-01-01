6e40526042a79bab928bc90a9999d0bf8face6dc
segundocristian219-oss <segundocristian219@gmail.com>
2026-01-01 16:40:32 -0600

    Actualizar hidetag.js

--- plugins/grupos/hidetag.js
+++ plugins/grupos/hidetag.js
@@ -2,7 +2,7 @@ import { generateWAMessageFromContent, downloadContentFromMessage } from '@whisk
 import fetch from 'node-fetch'
 
 let thumb = null
-fetch('https://files.catbox.moe/lnm43f.jpg')
+fetch('https://files.catbox.moe/5jj5xk.jpg')
   .then(r => r.arrayBuffer())
   .then(buf => thumb = Buffer.from(buf))
   .catch(() => null)
