c771ed7419c82759868a8e52737c50aba8682a00
segundocristian219-oss <segundocristian219@gmail.com>
2026-01-01 16:39:59 -0600

    Actualizar menu.js

--- plugins/menus/menu.js
+++ plugins/menus/menu.js
@@ -145,7 +145,7 @@ const handler = async (m, { conn }) => {
   await conn.sendMessage(
     m.chat,
     {
-      image: { url: "https://files.catbox.moe/lnm43f.jpg" },
+      image: { url: "https://files.catbox.moe/5jj5xk.jpg" },
       caption: menu,
       mentions: [m.sender]
     },
